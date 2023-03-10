57cf7820c7d6667a928a14d5b969ccb30e4acfee
EarthLight72100 <116321896+EarthLight72100@users.noreply.github.com>
2023-03-09 18:00:50 -0800

    Finished announcements and almost classes

--- jeson-new-app-main/lib/home.dart
+++ jeson-new-app-main/lib/home.dart
@@ -21,7 +21,7 @@ class _RandomWordsState extends State<RandomWords> {
   String _value = " ";
 
   Database db = Database();
-  List<Map>? classes;
+  List<Map>? courses;
   final ScrollController _scrollbarControllerClasses = ScrollController();
 
   @override
@@ -35,12 +35,17 @@ class _RandomWordsState extends State<RandomWords> {
     db.getCourses().then((data) {
       if (data != null) {
         setState(() {
-          classes = data;
+          courses = data;
         });
-        print(classes);
+        print(courses);
       }
     });
   }
+
+  StreamSubscription<DatabaseEvent>? announcementsListener;
+  StreamSubscription<DatabaseEvent>? userDataListener;
+  List<DataSnapshot> announcements = [];
+  List<DataSnapshot> classes = [];
 
   @override
   Widget build(BuildContext context) {
@@ -50,17 +55,42 @@ class _RandomWordsState extends State<RandomWords> {
     double deviceHeight = data.size.height;
 
     double containerHeightRatio = 0.23;
-    StreamSubscription<DatabaseEvent>? listener;
-    List<DataSnapshot> announcements = [];
 
-    DatabaseReference ref = FirebaseDatabase.instance.ref("announcements");
-    listener = ref.onValue.listen((event) async {
-      for (final child in event.snapshot.children) {
-        // Handle the post.
-        // print(child.key);
-        announcements.add(child);
-      }
-    });
+    if (announcementsListener == null) {
+      DatabaseReference ref = FirebaseDatabase.instance.ref("announcements");
+      announcementsListener = ref.onValue.listen((event) async {
+        for (final child in event.snapshot.children) {
+          // Handle the post.
+          print("TESTING");
+          print(child.key);
+          announcements.add(child);
+          setState(() {});
+        }
+      });
+    }
+
+    if (userDataListener == null) {
+      DatabaseReference ref =
+          FirebaseDatabase.instance.ref(AuthenticationHelper().user.uid);
+      userDataListener = ref.onValue.listen((event) async {
+        for (final child in event.snapshot.children) {
+          // Handle the post.
+          print("TESTING");
+          print(child.key);
+          // classes.add(child);
+          if (child.key == "classes") {
+            var items = child.value as List<Object?>;
+            for (final item in items) {
+              DataSnapshot info = item as DataSnapshot;
+              classes.add(info);
+            }
+          }
+          setState(() {});
+        }
+      });
+    }
+
+    // print(announcements);
 
     return Scaffold(
       appBar: AppBar(
@@ -147,9 +177,10 @@ class _RandomWordsState extends State<RandomWords> {
                             padding: EdgeInsets.only(
                                 left: 20, right: 20, top: 10, bottom: 10),
                             scrollDirection: Axis.vertical,
-                            children: [
-                              // announcements.map((item) => AnnouncementEntry(announcement: item)).toList()
-                            ],
+                            children: announcements
+                                .map((item) =>
+                                    AnnouncementEntry(announcement: item))
+                                .toList(),
                           )))),
                   SizedBox(height: SizeConfig.blockSizeVertical! * 3),
                   Text(
@@ -162,63 +193,53 @@ class _RandomWordsState extends State<RandomWords> {
                       child: Card(
                           color: Color.fromARGB(205, 255, 255, 255),
                           child: Container(
-                              // color: Color(0xffa80e0e),
-                              // height: deviceHeight * containerHeightRatio,
-                              // width: deviceWidth * 0.9,
-                              child: classes != null
-                                  ? Scrollbar(
-                                      controller: _scrollbarControllerClasses,
-                                      thumbVisibility: false,
-                                      child: ListView.separated(
-                                        shrinkWrap: true,
-                                        physics: ClampingScrollPhysics(),
-                                        itemCount: classes!.length,
-                                        itemBuilder:
-                                            (BuildContext context, int index) {
-                                          return Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(classes![index]['name']!),
-                                                Text(classes![index]['date']!),
-                                                Text(
-                                                    "Information: ${classes![index]['info']!}",
-                                                    maxLines: 5),
-                                              ],
-                                            ),
-                                          );
-                                        },
-                                        separatorBuilder:
-                                            (BuildContext context, int index) =>
-                                                const Divider(),
-                                      ),
-                                    )
-                                  : Center(
-                                      child: Text(
-                                        'No classes',
-                                        style: TextStyle(fontSize: 25),
-                                      ),
-                                    )))),
-                  // Text(
-                  //   "Calendar",
-                  //   style: TextStyle(fontSize: 24),
-                  // ),
-                  // Container(
-                  //     color: Colors.white,
-                  //     height: deviceHeight * containerHeightRatio,
-                  //     width: deviceWidth * 0.9,
-                  //     child: Column(
-                  //         crossAxisAlignment: CrossAxisAlignment.stretch,
-                  //         children: [
-                  //           //Feel free to customize the look of the font in any way you want
-                  //           Text("-Event 1: Date"),
-                  //           Text("-Event 2: Date"),
-                  //           Text("-Event 3: Date"),
-                  //         ])),
+                            child: ListView(
+                              padding: EdgeInsets.only(
+                                  left: 20, right: 20, top: 10, bottom: 10),
+                              scrollDirection: Axis.vertical,
+                              children: classes
+                                  .map((item) => ClassEntry(course: item))
+                                  .toList(),
+                            ),
+                            // child: courses != null
+                            //     ? Scrollbar(
+                            //         controller: _scrollbarControllerClasses,
+                            //         thumbVisibility: false,
+                            //         child: ListView.separated(
+                            //           shrinkWrap: true,
+                            //           physics: ClampingScrollPhysics(),
+                            //           itemCount: courses!.length,
+                            //           itemBuilder:
+                            //               (BuildContext context, int index) {
+                            //             return Padding(
+                            //               padding: const EdgeInsets.all(8.0),
+                            //               child: Column(
+                            //                 mainAxisAlignment:
+                            //                     MainAxisAlignment.start,
+                            //                 crossAxisAlignment:
+                            //                     CrossAxisAlignment.start,
+                            //                 children: [
+                            //                   Text(courses![index]['name']!),
+                            //                   Text(courses![index]['date']!),
+                            //                   Text(
+                            //                       "Information: ${courses![index]['info']!}",
+                            //                       maxLines: 5),
+                            //                 ],
+                            //               ),
+                            //             );
+                            //           },
+                            //           separatorBuilder:
+                            //               (BuildContext context, int index) =>
+                            //                   const Divider(),
+                            //         ),
+                            //       )
+                            //     : Center(
+                            //         child: Text(
+                            //           'No classes',
+                            //           style: TextStyle(fontSize: 25),
+                            //         ),
+                            //       )
+                          ))),
                 ],
               ),
             ),
@@ -235,6 +256,18 @@ class AnnouncementEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String title = "";
+    String description = "";
+    String date = "";
+    for (final child in announcement.children) {
+      if (child.key == "title") {
+        title = child.value as String;
+      } else if (child.key == "date") {
+        date = child.value as String;
+      } else if (child.key == "description") {
+        description = child.value as String;
+      }
+    }
     return SizedBox(
         width: SizeConfig.blockSizeHorizontal! * 75,
         height: 75,
@@ -245,10 +278,42 @@ class AnnouncementEntry extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Title, date"),
-                Text("Lorum ipsum dolor sit amet...")
-              ],
+              children: [Text("$title, $date"), Text(description)],
+            ),
+          ),
+        ));
+  }
+}
+
+class ClassEntry extends StatelessWidget {
+  final DataSnapshot course;
+  const ClassEntry({super.key, required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    String name = "";
+    String description = "";
+    String date = "";
+    for (final child in course.children) {
+      if (child.key == "name") {
+        name = child.value as String;
+      } else if (child.key == "date") {
+        date = child.value as String;
+      } else if (child.key == "description") {
+        description = child.value as String;
+      }
+    }
+    return SizedBox(
+        width: SizeConfig.blockSizeHorizontal! * 75,
+        height: 100,
+        child: Card(
+          color: Color.fromARGB(239, 255, 255, 255),
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Text("$name, $date"), Text(description)],
             ),
           ),
         ));
