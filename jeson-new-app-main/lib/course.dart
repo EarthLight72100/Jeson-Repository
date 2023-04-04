cfa262e60dcd62f11692a01d6108fca632384d7f
Andrew Park <joohyung122002@gmail.com>
2023-04-03 21:48:38 -0700

    Quick Fix
    -preparation for resubmission

--- jeson-new-app-main/lib/course.dart
+++ jeson-new-app-main/lib/course.dart
@@ -47,14 +47,14 @@ class CourseState extends State<CoursePage> {
             //     ),
             //   ),
             // ),
-            Card(
-              child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    maxLines: 10,
-                  )),
-            ),
+            // Card(
+            //   child: Padding(
+            //       padding: EdgeInsets.all(8.0),
+            //       child: Text(
+            //         "Enter course code",
+            //         maxLines: 10,
+            //       )),
+            // ),
             Form(
               key: _formKey,
               child: Column(
